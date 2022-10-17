b07e9dd4a6bdd2a3e81ae5a5ad8d67cffe8947b1
IgnacioVillarreal31 <110312930+IgnacioVillarreal31@users.noreply.github.com>
2022-10-17 11:54:21 -0300

    Add files via upload

---
+++ PII_Pipes_Filters_Start/src/Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CompAndDel.Pipes;
+using CompAndDel.Filters;
+using TwitterUCU;
+
+namespace CompAndDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+            provider.SavePicture(picture, @"luke0.jpg");
+            
+            // Parte 1
+            IPipe pipeNull = new PipeNull();
+            IFilter filterNegative = new FilterNegative();
+            IPipe pipeSerial = new PipeSerial(filterNegative,pipeNull);
+            IFilter filterGreyscale = new FilterGreyscale();
+            IPipe pipeSerial2 = new PipeSerial(filterGreyscale,pipeSerial);
+
+            picture = pipeSerial.Send(picture);
+            provider.SavePicture(picture, @"luke1.jpg");
+            picture = pipeSerial2.Send(picture);
+            provider.SavePicture(picture, @"luke2.jpg");
+            
+            var twitter = new TwitterImage();
+            Console.WriteLine(twitter.PublishToTwitter("Nada que hacer", @"PathToImage.png"));
+            
+        }
+    }
+}
